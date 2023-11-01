5e480e00257b9eeb5d1e2bc0040c4eb2b1104d8b
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-11-01 16:22:22 -0700

    8. To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if ord(i) >= 97 and ord(i) <= 122: 
+        if ord(i) >= 97 and ord(i) <= 122:
             i = ord(i) - 32
         else:
             i = ord(i)
