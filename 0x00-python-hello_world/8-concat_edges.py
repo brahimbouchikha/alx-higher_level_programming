e05062ee1211565fb5a11aaad10265494b711575
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-11-01 13:01:19 -0700

    8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = ' '.join((str.split(',')[2]).split()[0:2]) + " with Python"
-print(str)
+str = str.split(' ')
+print(' '.join([str[5], str[6], str[12], str[0]]))
