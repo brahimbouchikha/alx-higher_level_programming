43a2db924d68c3d7a486602402d1790b5b358c74
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-11-02 19:07:52 -0700

    3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from sys import argv
+from sys import argv, maxsize
 if __name__ == "__main__":
     _sum_ = 0
     for i in range(1, len(argv)):
         _sum_ += int(argv[i])
-    print("{:d}".format(_sum_))
+    if abs(_sum_) <= maxsize:
+        print("{}".format(_sum_))
 
