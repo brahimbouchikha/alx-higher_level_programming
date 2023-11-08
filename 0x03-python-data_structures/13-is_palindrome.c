ac116466a45216a315495fe55aa98491d0935e34
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-11-07 16:28:20 -0800

    13 is palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,22 @@
 #include "lists.h"
 
 /**
+ * freelist - free list
+ * @head: list that will be free
+ */
+
+void freelist(listint_t *head)
+{
+	listint_t *tmp;
+
+	while(head)
+	{
+		tmp = head;
+		free(tmp);
+		head = head->next;
+	}
+}
+/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: the list to check
  *
@@ -20,21 +36,27 @@ int is_palindrome(listint_t **head)
 		fast = fast->next->next;
 		slow = slow->next;
 	}
+	slow = slow->next;
 	/* copy main Linked list to middle "*/
 	while (slow != NULL)
 	{
-		ptr = malloc(sizeof(listint_t))
+		ptr = malloc(sizeof(listint_t));
+		if (ptr == NULL)
+		{
+			freelist(ptr);
+			return (0);
+		}
+		ptr->n = slow->n;
 		ptr->next = middle;
 		middle = ptr;
 		slow = slow->next;
 	}
-	free(ptr);
-	ptr1 = middle->next;
+	ptr1 = middle;
 	while (ptr1 != NULL)
 	{
 		if (tmp->n != ptr1->n)
 			return (0);
-		ptr1 = ptr->next;
+		ptr1 = ptr1->next;
 		tmp = tmp->next;
 	}
 	return (1);
