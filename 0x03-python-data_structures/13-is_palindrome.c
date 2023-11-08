7afca1b1b2cdb3a379ab925140ce49396c4af0b8
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-11-07 16:01:17 -0800

    13 is palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,40 +6,36 @@
  *
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
-
 int is_palindrome(listint_t **head)
 {
-listint_t *tmp = *head;
-listint_t *fast = *head;
-listint_t *slow = *head;
-listint_t *middle = NULL, *ptr;
+	listint_t *tmp = *head;
+	listint_t *fast = *head;
+	listint_t *slow = *head;
+	listint_t *middle = NULL, *ptr = NULL, *ptr1 = NULL;
 
-if (*head == NULL || (*head)->next == NULL)
-return (1);
-while (fast->next && fast->next->next)
-{
-fast = fast->next->next;
-slow = slow->next;
-}
-/* copy main Linked list to middle "*/
-while (slow != NULL)
-{
-ptr = malloc(sizeof(listint_t));
-ptr->next = middle;
-ptr->n = slow->n;
-middle = ptr;
-slow = slow->next;
-}
-free(ptr);
-ptr = middle->next;
-while (ptr != NULL)
-{
-if (tmp->n != ptr->n)
-{
-return (0);
-}
-ptr = ptr->next;
-tmp = tmp->next;
-}
-return (1);
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+	while (fast->next && fast->next->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+	/* copy main Linked list to middle "*/
+	while (slow != NULL)
+	{
+		ptr = malloc(sizeof(listint_t))
+		ptr->next = middle;
+		middle = ptr;
+		slow = slow->next;
+	}
+	free(ptr);
+	ptr1 = middle->next;
+	while (ptr1 != NULL)
+	{
+		if (tmp->n != ptr1->n)
+			return (0);
+		ptr1 = ptr->next;
+		tmp = tmp->next;
+	}
+	return (1);
 }
