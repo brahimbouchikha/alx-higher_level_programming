ee662fe5aa434a5de9ce2c7d63e94532861e9511
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-11-21 17:46:20 -0800

    2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+
+class Square:
+    """Define a square"""
+
+    def __init__(self, size=0):
+        """Constractor.
+
+        Args:
+            size: size of a side of square
+
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
