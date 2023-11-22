f3fb07d4915341b5721e62c7f96ee7bf00a9faef
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-11-21 18:12:18 -0800

    4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -46,8 +46,8 @@ class Square:
             ValueError: if size is less than 0
 
         """
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self._size = value
