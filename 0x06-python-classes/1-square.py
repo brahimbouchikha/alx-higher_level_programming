69501f897dff5e6ec8a6c058f41b521d6a9f99d9
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-11-21 17:25:18 -0800

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+
+class Square:
+    """Define a square"""
+    def __init__(self, size):
+        """Constractor.
+
+        Args:
+            size: size of a side of square
+        """
+        self.__size = size
