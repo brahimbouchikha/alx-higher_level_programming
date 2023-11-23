324c4b8d67d46529c4219ed31a8a0ec790b487b1
brahim bouchikha <brahim.bouchikha@gmail.com>
2023-11-22 17:54:16 -0800

    6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -79,7 +79,8 @@ class Square:
             return
         [print("") for i in range(0, self.__position[1])]
         for i in range(self.size):
-            [print(" ", end="") for k in range(0, self.__position[0])]
+            for k in range(self.__position[0]):
+                    print(" ", end="")
             for j in range(self.size):
                 print("#", end='')
             print()
