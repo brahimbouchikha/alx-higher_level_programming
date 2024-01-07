714c545a44ea74fa03e3c934bf00885783161604
ibrahim <ibrahim@localhost.localdomain>
2024-01-07 02:04:50 +0100

    9

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -60,7 +60,7 @@ class Rectangle:
 
     @classmethod
     def square(cls, size = 0):
-        return Rectangle(size, size)
+        return cls(size, size)
 
     def __str__(self):
         total = ""
