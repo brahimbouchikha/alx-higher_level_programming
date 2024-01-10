68bced5e922131a238dc4d34364f6705b9fa3b18
ibrahim <ibrahim@localhost.localdomain>
2024-01-10 02:01:15 +0100

    3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defind To_json_string function """
+
+
+import json
+
+def to_json_string(my_obj):
+    """Returns Json representation of an object string"""
+    return json.loads(obj)
