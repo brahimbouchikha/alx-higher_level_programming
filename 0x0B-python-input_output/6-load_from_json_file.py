5a10b7a1d63aaa4e21bfacdfb96542ef36df3725
ibrahim <ibrahim@localhost.localdomain>
2024-01-10 02:17:22 +0100

    6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 
-"""Define load_from_json_file function"""
+"""Define load_from_json_file function."""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Create an Object from a json file."""
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(filename)
