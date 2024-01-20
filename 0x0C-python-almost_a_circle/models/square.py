2285ff66b3e3fd4109b2fafefd78894eabb94ba7
ibrahim <ibrahim@localhost.localdomain>
2024-01-20 02:17:44 +0100

    0

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,7 +18,7 @@ class Square(Rectangle):
     @property
     def size(self):
         '''size of this square'''
-        return self.size
+        return self.width
 
     @size.setter
     def size(self, value):
