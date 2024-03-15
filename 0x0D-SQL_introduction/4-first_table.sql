bb769ef3ff4bd1d2fb728c420532ab8c6e45bc7c
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-03-15 01:50:08 +0000

    4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- Create a first_table in database
 -- Execute: cat 4-first_table.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-CREATE DATABASE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
