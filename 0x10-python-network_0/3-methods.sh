ab22c1bd3a1a28384451c5f50f959e4e50fb47d6
brahim bouchikha <brahim.bouchikha@gmail.com>
2024-07-10 01:51:32 +0100

    all

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL and displays all HTTP methods the server will accept
-curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -f2- -d" "
+# script to get the allowed methods in an server if availaible through OPTIONS http request
+curl -s -I -X OPTIONS "$1" | grep 'Allow:' | cut -f2- -d' '
